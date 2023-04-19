894b44bee8230951fab9f6bdc8094edcc260cd99
Nafees Nehar <nafees87n@gmail.com>
2023-04-19 14:03:07 +0530

    Update background.js

--- inboxsdk/src/background.js
+++ inboxsdk/src/background.js
@@ -1,21 +1,2 @@
 import "@inboxsdk/core/background";
-// Load the Gmail API
-// var gapi = window.gapi;
-
-// // Load the InboxSDK library
-// // var sdk = require("inboxsdk");
-
-// // Initialize the Gmail API
-// gapi.load("client:auth2", function() {
-//   gapi.client.init({
-//     apiKey: "AIzaSyCXfHPh2iY9i5WgO3ZIcUyGMMHFPmRZxcs",
-//     clientId: "YOUR_CLIENT_ID",
-//     discoveryDocs: [
-//       "https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest"
-//     ],
-//     scope: "https://www.googleapis.com/auth/gmail.modify"
-//   });
-
-//   // Authorize the Gmail API
-//   gapi.auth2.getAuthInstance().signIn();
-// });
+// Start here
